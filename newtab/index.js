9b5b8366af994aadda73b1a536958c35df2cb38f
Alexander Griffin <alexander.griffin99@hotmail.com>
2018-04-13 22:16:37 -0400

    rethought some shit and removed the whole todo list thing, this should be about fullscreen functionality not things easily done with other websites

--- newtab/index.js
+++ newtab/index.js
@@ -1,3 +1,7 @@
 (() => {
-
+  document.getElementById("searchbar").addEventListener("keydown", () => {
+    if (event.keyCode == 13 || event.which == 13) {
+      location='http://www.google.com/search?q=' + encodeURIComponent(document.getElementById('searchbar').value)
+    }
+  })
 })()
