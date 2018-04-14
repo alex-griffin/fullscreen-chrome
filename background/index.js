b0d37f92421367c6104bb80970446789ac165894
Alexander Griffin <alexander.griffin99@hotmail.com>
2018-04-13 21:40:19 -0400

    got fucking fullscreen working

--- background/index.js
+++ background/index.js
@@ -1,7 +1,3 @@
-var counter = 0;
-chrome.browserAction.onClicked.addListener(function (tab) {
-    counter++;
-    if (counter == 5) {
-        alert("Hey !!! You have clicked five times");
-    }
-});
+chrome.windows.onCreated.addListener((window) => {
+  chrome.windows.update(window.id, { state: "fullscreen" })
+})
