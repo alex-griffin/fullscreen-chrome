d69ca90b6066b0ea2e00664d55798fcbee83847d
Alexander Griffin <alexander.griffin99@hotmail.com>
2018-04-13 22:20:07 -0400

    _

--- newtab/index.js
+++ newtab/index.js
@@ -1,7 +1,10 @@
 (() => {
+  const searchbar = document.getElementById("searchbar")
   document.getElementById("searchbar").addEventListener("keydown", () => {
     if (event.keyCode == 13 || event.which == 13) {
       location='http://www.google.com/search?q=' + encodeURIComponent(document.getElementById('searchbar').value)
     }
   })
+
+  searchbar.autofocus = true
 })()
