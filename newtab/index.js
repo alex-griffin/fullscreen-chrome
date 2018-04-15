51554dcc3dbe63cf66a68369041260fc16f89cfd
Alexander Griffin <alexander.griffin99@hotmail.com>
2018-04-14 21:16:03 -0400

    s

--- newtab/index.js
+++ newtab/index.js
@@ -2,7 +2,12 @@
   const searchbar = document.getElementById("searchbar")
   document.getElementById("searchbar").addEventListener("keydown", () => {
     if (event.keyCode == 13 || event.which == 13) {
-      location='http://www.google.com/search?q=' + encodeURIComponent(document.getElementById('searchbar').value)
+      const value = e.target.value
+      if(value.match(^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$) {
+        location = value
+      } else {
+        location = 'http://www.google.com/search?q=' + encodeURIComponent(value)
+      }
     }
   })
 
@@ -12,8 +17,8 @@
     const dateContainer = document.getElementById("date-container")
     const date = new Date()
     let h = date.getHours()
-    h -= (h > 12 ? h - 10 : 0)
-    const m = date.getMinutes()
+    h -= (h > 12 ? 12 : 0)
+    const m = date.getMinutes().toString().padStart(2, "0")
     dateContainer.innerHTML = h + ":" + m
     setTimeout(getTime, 500);
   }
